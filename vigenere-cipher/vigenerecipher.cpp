043eef5bcf886e7c9c7515d9b1f3180444a01326
nawangilmi <64404123+nawangilmi@users.noreply.github.com>
2022-10-11 19:33:04 +0700

    Add files via upload

---
+++ vigenere-cipher/vigenerecipher.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vigenere {
+
+ public:
+  string key;
+  Vigenere(string key) {
+   for(int i = 0; i < key.size();++i)
+   {
+    if (key[i] >= 'A' && key [i] <= 'Z')
+    this->key += key [i];
+   else if (key[i] >= 'a' && key[i] <= 'z')
+    this->key += key [i] + 'A' - 'a';
+   }
+  }
+
+  string enkripsi(string text){
+   string out;
+   
+   for (int i = 0, j = 0; i < text.length(); ++i){
+   char c = text[i];
+   
+  if (c >= 'a' && c<= 'z')
+   c += 'A' - 'a';
+  else if ( c < 'A' || c> 'Z')
+   continue;
+   
+  out += (c +key[j] -2 * 'A') %26 +'A';
+  j = (j +1) % key.length();
+  }
+  
+  return out;
+ }
+ 
+ string dekripsi(string text){
+  string out;
+  
+  for (int i = 0, j = 0; i < text.length(); ++i)
+   {
+   char c = text[i];
+   
+  if (c >= 'a' && c<= 'z')
+   c += 'A' - 'a';
+  else if ( c < 'A' || c> 'Z')
+   continue;
+   
+  out += (c - key[j] +26 ) %26 +'A';
+  j = (j +1) % key.length();
+  }
+  
+  return out;
+ }
+};
+
+int main(){
+string a,b;
+cout<<"Masukkan Plaintext : ";
+cin>>a;
+cout<<"Masukkan Key : ";
+cin>>b;
+ string original=a;
+ Vigenere chiper=b;
+ 
+ string ciphertext = chiper.enkripsi(original);
+ string dekripsi = chiper.dekripsi(ciphertext);
+ cout<<endl;
+ cout<<"terenkripsi :"<<ciphertext<<endl;
+ cout<<"terdekripsi :"<<dekripsi<<endl;
+}
